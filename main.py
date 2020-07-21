e5a84d5bd5b1e0b0a0aa41b76ab04c353bf95fbd
robberuda <robberuda@gmail.com>
2020-07-21 15:50:45 +0200

    adjust code and comments

--- main.py
+++ main.py
@@ -3,40 +3,39 @@ import numpy as np
 from palette import colors
 import time
 import os
-THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
+THIS_FOLDER = os.path.dirname(os.path.abspath(__file__)) # init path (pyCharm)
 
-cap = cv.VideoCapture(0) #webcam n#0
-whT = 320 #risoluzione quadrata
-confThreshold = 0.4 # soglia riconoscimento
-nmsThreshold = 0.2  #non maximum suppression,fattore che elimina i box di troppo
+cap = cv.VideoCapture(0) # webcam n#0
+whT = 320                # use a sqare image
+confThreshold = 0.5      # our value of threshold
+nmsThreshold = 0.2       # non maximum suppression
  
-#### LOAD MODEL
-## Coco Names
+
+# Load Coco Names of classes
 classesFile = os.path.join(THIS_FOLDER,"coco.names.txt" )
 classNames = []
 with open(classesFile, 'rt') as f:
     classNames = f.read().rstrip('\n').split('\n')
+
 print(classNames)
-## Model Files
+
+# Model Files downloaded from YOLO website https://pjreddie.com/darknet/yolo/:
 modelConfiguration = os.path.join(THIS_FOLDER,"yolov3-320.cfg")
 modelWeights = os.path.join(THIS_FOLDER,"yolov3-320.weights")
 
-
-"""creazione della rete convoluzionale con il file .cfg di configurazione
-   e il file dei pesi di YOLO preso dal sito """
+''' create the CNN with ".cfg" configuration file and ".weights" weights of 
+    train '''
 net = cv.dnn.readNetFromDarknet(modelConfiguration, modelWeights)
 net.setPreferableBackend(cv.dnn.DNN_BACKEND_OPENCV) #scelta backend (software)
 net.setPreferableTarget(cv.dnn.DNN_TARGET_CPU)      #scelta target  (hardware)
 
 
-""" questa funzione prende in input un immagine e restituisce l'immagine con i
-    riquadri che classificano gli oggetti, con l'etichetta e la precisione """
+''' this function take in input the output of CNN and create boxes over image '''
 def findObjects(outputs,img):
-    hT, wT, cT = img.shape #dimensioni immagine in input
-    
-    bbox = []    #list of boxes
-    classIds = []#list of ids
-    confs = []   #list of confidences
+    hT, wT, cT = img.shape # image's dimension
+    bbox = []              # list of boxes
+    classIds = []          # list of ids
+    confs = []             # list of confidences
     
     for output in outputs:
         for det in output:
@@ -46,60 +45,52 @@ def findObjects(outputs,img):
             if confidence > confThreshold:
                 w,h = int(det[2]*wT) , int(det[3]*hT)
                 x,y = int((det[0]*wT)-w/2) , int((det[1]*hT)-h/2)
-                """aggiunge alla lista 'bbox' i box che saranno centrati in x 
-                e y, e avranno dimensione w e h.
-                Inoltre aggiunge alle liste 'classIds' e 'confs' gli id degli 
-                oggetti nell'immagine e le relative confidenze """
+                ''' use de output vector of yolo for add a box in 'bbox' list: id of 
+                object, dimension of box, cordinates in the image and the confidance '''
                 bbox.append([x,y,w,h])
                 classIds.append(classId)
                 confs.append(float(confidence))
                 
     indices = cv.dnn.NMSBoxes(bbox, confs, confThreshold, nmsThreshold)
-    
-    """ """
+
     for i in indices:
-        i = i[0] # i = quanti oggetti nella figura
+        i = i[0]  #i = indicate how much object in the image
         box = bbox[i]
         x, y, w, h = box[0], box[1], box[2], box[3]
+        color = colors[classIds[i]] #load different colors for boxes
 
-        color = colors[classIds[i]] #cambia colore per ogni oggetto in figura
-        
-        labelLenght = len(f'{classNames[classIds[i]].upper()}')
-        cv.rectangle(img, (x-1, y), (x+11*labelLenght+62,y-25), color, -2) #rettangolo nome
-        
+        '''add  box and lable'''
         cv.rectangle(img, (x, y), (x+w,y+h), color, 2)
+        labelLenght = len(f'{classNames[classIds[i]].upper()}')
+        cv.rectangle(img, (x-1, y), (x+11*labelLenght+62,y-25), color, -2)
         cv.putText(img,f'{classNames[classIds[i]].upper()} {int(confs[i]*100)}%',
                   (x+2, y-7), cv.FONT_HERSHEY_SIMPLEX, 0.6, (255, 255, 255), 2)
- 
- 
     
 if __name__=="__main__": 
     start_time = time.time()
     nFps = 6
-    frame= 0
-    fps='barbabietola'
-    
-    while True:
-        success, img = cap.read()
-     
-        blob = cv.dnn.blobFromImage(img, 1 / 255, (whT, whT),
-                                    [0, 0, 0], 1, crop=False)
-        net.setInput(blob)
-        layersNames = net.getLayerNames()
-        outputNames = [(layersNames[i[0] - 1]) for i in net.getUnconnectedOutLayers()]
-        outputs = net.forward(outputNames)
-        # print (net)
-        findObjects(outputs,img)
-        
+    frame = 0
+    fps = 'initialization FPS'
 
+    while True:
+        success, img = cap.read()#capture image by webcam
+        #convert image in blob (binary large object)
+        blob = cv.dnn.blobFromImage(img, 1 / 255, (whT, whT), [0, 0, 0], 1, crop=False)
+        net.setInput(blob)
+        layersNames = net.getLayerNames() #take name of all network's layers
+        # getUnconnectedOutLayers take index of out layers of CNN, in particular are three: 200, 227 and 254(last layer)
+        outputNames = [(layersNames[i[0] - 1]) for i in net.getUnconnectedOutLayers()]
+        outputs = net.forward(outputNames) #outputsNames=(yolo_82,yolo94,yolo106)
+        findObjects(outputs,img)
         frame += 1
         TIME = time.time() - start_time
+
         if frame > nFps:
             fps='FPS: '+str("{0:.2f}".format(frame/TIME))
             # print("FPS:", frame/TIME)
             frame = 0
             start_time = time.time()
-        
-        cv.putText(img, fps, (2, 18), cv.FONT_HERSHEY_SIMPLEX, 0.6, (200, 200, 200), 2)
-        cv.imshow('Image', img) #show new image with 
+
+        cv.putText(img, fps, (2, 18), cv.FONT_HERSHEY_SIMPLEX, 0.6, (50, 50, 50), 2)
+        cv.imshow('Image', img) #show new image with
         cv.waitKey(1)
